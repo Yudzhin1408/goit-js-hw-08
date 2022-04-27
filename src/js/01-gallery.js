a2afc8df9200806df03bcd4119d7dd588410539b
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-04-27 23:09:02 +0300

    дз1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,23 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import SimpleLightbox from "simplelightbox"
 
-console.log(galleryItems);
 const galleryEl = document.querySelector('.gallery');
 
 
 const imagesEl = galleryItems.map(({ preview, original, description }) =>
-  `<li>
+  `
     <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
-</li>`
+`
 ).join('');
     
-
-
 galleryEl.insertAdjacentHTML('beforeend',imagesEl);
 
 // open
