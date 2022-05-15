7e1e151e0107f26bb0348dfc7ca2af717345b0db
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-05-15 19:31:20 +0300

    дз-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from "@vimeo/player";
+const throttle = require('lodash.throttle');
 
+const iframeEl = document.querySelector('iframe');
+
+const player = new Player(iframeEl);
+
+let savedCurrentTime = 0;
+populateTime();
+
+const onPlay = function (data) {
+    localStorage.setItem("video-time", data.seconds);
+
+};
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function populateTime() {
+    const currentTime = localStorage.getItem("videoplayer-current-time");
+    if (currentTime) {        
+        savedCurrentTime = currentTime;
+    }
+}
+
+ player.setCurrentTime(savedCurrentTime);
